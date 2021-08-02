d56cf328d3c8cb2806f57b60bc9f4b51ab895993
Kathryn Wheeler <kiwheel@bu.edu>
2021-08-02 17:02:49 -0400

    debugging

--- forecastMain.R
+++ forecastMain.R
@@ -4,7 +4,7 @@ library(rjags)
 library(runjags)
 library(doParallel)
 library(ncdf4)
-remove.packages('usethis')
+#remove.packages('usethis')
 library(devtools, lib.loc = "/share/pkg.7/r/3.5.1/install/lib64/R/library")
 
 ##' Calculates cummulative Tair (within one year for one ensemble)
